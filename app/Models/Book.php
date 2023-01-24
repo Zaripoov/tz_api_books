ba5a5bd0511ea3e60da29d992db2c79c0ff3c00e
Zaripov <ruzal.zarip1324@gmail.com>
2023-01-24 20:03:25 +0300

    ide helper model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Barryvdh\LaravelIdeHelper\Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $isbn
+ * @property int $year
+ * @property int $author_id
+ * @method static Builder|Book newModelQuery()
+ * @method static Builder|Book newQuery()
+ * @method static Builder|Book query()
+ * @method static Builder|Book whereAuthorId($value)
+ * @method static Builder|Book whereId($value)
+ * @method static Builder|Book whereIsbn($value)
+ * @method static Builder|Book whereTitle($value)
+ * @method static Builder|Book whereYear($value)
+ * @mixin Eloquent
+ */
 class Book extends Model
 {
     use HasFactory;
--- app/Models/BookAuthor.php
+++ app/Models/BookAuthor.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Barryvdh\LaravelIdeHelper\Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\BookAuthor
+ *
+ * @property int $id
+ * @property string $name
+ * @method static Builder|BookAuthor newModelQuery()
+ * @method static Builder|BookAuthor newQuery()
+ * @method static Builder|BookAuthor query()
+ * @method static Builder|BookAuthor whereId($value)
+ * @method static Builder|BookAuthor whereName($value)
+ * @mixin Eloquent
+ */
 class BookAuthor extends Model
 {
     use HasFactory;
